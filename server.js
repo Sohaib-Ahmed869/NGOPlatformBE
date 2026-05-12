618b2d0abb1eb75be902f19127d8f49b50d2c47c
asadawan16 <asadawan16900@gmail.com>
2026-05-12 15:19:38 +0500

    client url for cors

--- server.js
+++ server.js
@@ -39,13 +39,15 @@ scheduleSubscriptionChecks();
 // Middleware
 // app.use(cors({ origin: '*' }));
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  process.env.CLIENT_URL,
+].filter(Boolean);
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://shahidafridifoundation.org.au",
-      "https://www.shahidafridifoundation.org.au",
-    ],
+    origin: allowedOrigins,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
